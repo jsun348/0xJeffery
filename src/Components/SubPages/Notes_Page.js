f921d6c35759e8b2421fe681d691dcd6fa6a6b61
Jeffery Sun <jefferysun1993@gmail.com>
2023-12-30 02:55:06 -0500

    fix build

--- src/Components/SubPages/Notes_Page.js
+++ src/Components/SubPages/Notes_Page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotesPage = ({ children }) => {
   return (
     <div className='flex flex-col container'>
-        <a href="/" >Main page</a>
+        <a href="%PUBLIC_URL%/" >Main page</a>
         <div className="my-4">
           {children}
         </div>
